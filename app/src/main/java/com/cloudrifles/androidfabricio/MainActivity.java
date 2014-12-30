6723745715837028ee0b50b74d3be10665701b9f
cloudrifles <cloudrifles@cloudrifles.com>
2014-12-31 01:41:32 +0700

    Initial commit

---
+++ app/src/main/java/com/cloudrifles/androidfabricio/MainActivity.java
@@ -0,0 +1,50 @@
+package com.cloudrifles.androidfabricio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import com.mopub.common.MoPub;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "ye9K3QojeFfiK97bngkBZvQCM";
+    private static final String TWITTER_SECRET = "mOCLpILx6sp3mjxPciIKfYJWG9BfRnwlsG1lGIL04m2zjT4M8H";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);Fabric.with(this, new Crashlytics(), new Twitter(authConfig), new MoPub());
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
